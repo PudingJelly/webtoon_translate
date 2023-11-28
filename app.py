a8d4b93f04d87d5b3bf665404bca7ed529fc21e2
Changju Mun <changju7857@gmail.com>
2023-11-28 16:27:50 +0900

    23.11.28 psd to png converting, browser printing

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from io import BytesIO
 import os
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request
 from googletrans import Translator
 from psd_tools import PSDImage
 from PIL import Image, ImageDraw, ImageFont
@@ -13,6 +12,8 @@ translator = Translator()
 def index():
     return render_template(
         "index.html",
+        image_data=None,
+        remove_text_image_data=None,
         all_layer_info="",
         all_extracted_info_list="",
         extracted_text_data="",
@@ -20,21 +21,23 @@ def index():
     )
 
 
-@app.route("/extract", methods=["POST"])
+@app.route("/conversion", methods=["POST"])
 def extract_text():
     # HTML에 출력하기 위한 리스트 초기화
     all_layer_info = []
     all_extracted_info_list = []
     extracted_text_data = []
     translated_text_data = []
+    converted_image_path = None
 
     try:
         source_files = request.files.getlist("source_file")
         for source_file in source_files:
-            source = PSDImage.open(source_file, encoding="cp1252")
+            source = PSDImage.open(source_file)
 
             if source:
-                all_layer_info = vars(source)  # 레이어의 정보
+                # all_layer_info = vars(source)  # 레이어의 정보
+                all_layer_info = []  # 레이어의 정보
 
                 # PSD 파일의 이미지 추출
                 image = source.compose()
@@ -42,20 +45,27 @@ def extract_text():
                 # 원본 파일 이름 추출
                 original_filename = os.path.splitext(source_file.filename)[0]
 
-                # 파일 저장 경로 및 네이밍
+                # 프로젝트 경로에 이미지 저장
                 remove_text_save_path = (
-                    "C:/Users/remis/Desktop/test-images/remove_text_test/remove_text/"
+                    f"static/images/remove-text/{original_filename}_remove_text.png"
                 )
-                remove_text_base_name = f"{original_filename}_remove_text.png"
+                # remove_text_base_name = f"{original_filename}_remove_text.png"
+                translated_text_save_path = f"static/images/translated-text/{original_filename}_translated_text.png"
+                # translated_text_base_name = f"{original_filename}_translated_text.png"
 
-                translated_text_save_path = "C:/Users/remis/Desktop/test-images/remove_text_test/translated_text/"
-                translated_text_base_name = f"{original_filename}_translated_text.png"
+                # 원본 변환 후 이미지 저장
+                converted_image_path = (
+                    f"static/images/original-converted/{original_filename}.png"
+                )
+                # converted_image_base_name = f"{original_filename}.png"
+                image.save(converted_image_path)
 
                 # 텍스트 레이어 추출을 위한 리스트 초기화
                 text_layer_info = []
 
                 # 모든 하위 레이어들을 찾아서 텍스트 레이어만 추출
                 for layer in source.descendants(include_clip=True):
+                    all_layer_info.append(layer)
                     if layer.kind == "type":
                         text_layer_info.append(
                             {
@@ -72,11 +82,11 @@ def extract_text():
                 all_extracted_info_list.append(text_layer_info)
 
                 # 이미지 분리를 위한 원본소스 복사
-                # remove_text_image = image.copy()
+                remove_text_image = image.copy()
                 translated_text_image = image.copy()
 
                 # font_size = 100  # 폰트 크기 설정
-                font = ImageFont.truetype("arial.ttf", 90)  # 사용할 폰트와 크기 설정
+                font = ImageFont.truetype("arial.ttf", 70)  # 사용할 폰트와 크기 설정
 
                 # 텍스트 레이어의 좌표, 크기 세분화 추출
                 # detail_info =[]
@@ -89,31 +99,34 @@ def extract_text():
                         layer["size"][0],
                         layer["size"][1],
                     )
-                    extracted_text_data.append(layer["text"])
-                    clean_text = text.replace('\r', '\n')
-                    print(clean_text)
+                    extracted_text_data.append(text)
+                    clean_text = text.replace("\r", "\n")
+                    # print(clean_text)
                     translated_name = translator.translate(
                         clean_text, src="ko", dest="en"
                     ).text
-                    print(translated_name)
+                    # print(translated_name)
 
-                    # if remove_text_image:
-                    #     white_box = Image.new("RGB", (width, height), color="white")
-                    #     remove_text_image.paste(white_box, (left, top))
-                    
-                    white_box = Image.new("RGB", (width+170, height+80), color="white")
-                    draw = ImageDraw.Draw(white_box)
-                    draw.text((0, 10), translated_name, fill="black", font=font)
-                    translated_text_image.paste(white_box, (left-100, top-50))
+                    if remove_text_image:
+                        white_box = Image.new("RGB", (width, height), color="white")
+                        remove_text_image.paste(white_box, (left, top))
 
-                    # 번역내용 화면에 출력
-                    # translated_text_data.append(translated_text)
+                    if translated_text_image:
+                        # 말풍선 지우고 번역하여 채우기
+                        text_box = Image.new(
+                            "RGB", (width + 80, height + 60), color="white"
+                        )
+                        # text_length = draw.textlength(translated_name, font=font)
+                        draw = ImageDraw.Draw(text_box)
+                        draw.text((0, 0), translated_name, fill="black", font=font)
+                        translated_text_image.paste(text_box, (left - 40, top - 10))
+
+                    # 번역내용 출력
+                    translated_text_data.append(translated_name)
 
                 # 이미지 저장
-                # remove_text_image.save(remove_text_save_path + remove_text_base_name)
-                translated_text_image.save(
-                    translated_text_save_path + translated_text_base_name
-                )
+                remove_text_image.save(remove_text_save_path)
+                translated_text_image.save(translated_text_save_path)
 
     except Exception as e:
         all_layer_info = []
@@ -123,6 +136,8 @@ def extract_text():
 
     return render_template(
         "index.html",
+        image_data=converted_image_path,
+        remove_text_image_data=remove_text_save_path,
         all_layer_info=all_layer_info,
         all_extracted_info_list=all_extracted_info_list,
         extracted_text_data=extracted_text_data,
--- backup/backup2.py
+++ backup/backup2.py
@@ -79,3 +79,180 @@ def extract_text():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    
+@app.route("/extract", methods=["POST"])
+def extract_text():
+    # HTML에 출력하기 위한 리스트 초기화
+    all_layer_info = []
+    all_extracted_info_list = []
+    extracted_text_data = []
+    translated_text_data = []
+
+    try:
+        source_files = request.files.getlist("source_file")
+        for source_file in source_files:
+            source = PSDImage.open(source_file)
+
+            if source:
+                # all_layer_info = vars(source)  # 레이어의 정보
+                all_layer_info = []  # 레이어의 정보
+
+                # PSD 파일의 이미지 추출
+                image = source.compose()
+
+                # 원본 파일 이름 추출
+                original_filename = os.path.splitext(source_file.filename)[0]
+
+                # 파일 저장 경로 및 네이밍
+                remove_text_save_path = (
+                    "C:/Users/remis/Desktop/test-images/remove_text_test/remove_text/"
+                )
+                remove_text_base_name = f"{original_filename}_remove_text.png"
+
+                translated_text_save_path = "C:/Users/remis/Desktop/test-images/remove_text_test/translated_text/"
+                translated_text_base_name = f"{original_filename}_translated_text_en.png"
+
+                # 텍스트 레이어 추출을 위한 리스트 초기화
+                text_layer_info = []
+
+                # 모든 하위 레이어들을 찾아서 텍스트 레이어만 추출
+                for layer in source.descendants(include_clip=True):
+                    all_layer_info.append(layer)
+                    if layer.kind == "type":
+                        text_layer_info.append(
+                            {
+                                "kind": layer.kind,
+                                "name": layer.name,
+                                "text": layer.text,
+                                "top": layer.top,
+                                "left": layer.left,
+                                "size": layer.size,
+                            }
+                        )
+                # 텍스트 레이어 정보 추출
+                extracted_info = text_layer_info
+                all_extracted_info_list.append(text_layer_info)
+
+                # 이미지 분리를 위한 원본소스 복사
+                remove_text_image = image.copy()
+                translated_text_image = image.copy()
+
+                # font_size = 100  # 폰트 크기 설정
+                font = ImageFont.truetype("arial.ttf", 70)  # 사용할 폰트와 크기 설정
+
+                # 텍스트 레이어의 좌표, 크기 세분화 추출
+                # detail_info =[]
+                for layer in extracted_info:
+                    name, text, top, left, width, height = (
+                        layer["name"],
+                        layer["text"],
+                        layer["top"],
+                        layer["left"],
+                        layer["size"][0],
+                        layer["size"][1],
+                    )
+                    extracted_text_data.append(text)
+                    clean_text = text.replace('\r', '\n')
+                    # print(clean_text)
+                    translated_name = translator.translate(
+                        clean_text, src="ko", dest="en"
+                    ).text
+                    # print(translated_name)
+
+                    if remove_text_image:
+                        white_box = Image.new("RGB", (width, height), color="white")
+                        remove_text_image.paste(white_box, (left, top))
+
+                    if translated_text_image:
+                        # 말풍선 텍스트 지우기
+                        # white_box = Image.new("RGB", (width, height), color="white")
+                        # translated_text_image.paste(white_box, (left, top))
+                        # 말풍선 지우고 번역하여 채우기
+                        text_box = Image.new("RGB", (width+70, height+60), color="white")
+                        # text_length = draw.textlength(translated_name, font=font)
+                        draw = ImageDraw.Draw(text_box)
+                        draw.text((0, 0), translated_name, fill="black", font=font)
+                        translated_text_image.paste(text_box, (left-30, top-10))
+
+                    # 번역내용 화면에 출력
+                    translated_text_data.append(translated_name)
+
+                # 이미지 저장
+                remove_text_image.save(remove_text_save_path + remove_text_base_name)
+                translated_text_image.save(
+                    translated_text_save_path + translated_text_base_name
+                )
+
+    except Exception as e:
+        all_layer_info = []
+        all_extracted_info_list = f"Error extracting text: {str(e)}"
+        extracted_text_data = []
+        translated_text_data = []
+
+    return render_template(
+        "index.html",
+        all_layer_info=all_layer_info,
+        all_extracted_info_list=all_extracted_info_list,
+        extracted_text_data=extracted_text_data,
+        translated_text_data=translated_text_data,
+    )
+
+# @app.route("/conversion", methods=["POST"])
+# def conversion():
+#     all_layer_info = []
+#     all_extracted_info_list = []
+#     extracted_text_data = []
+#     translated_text_data = []
+
+#     try:
+#         source_files = request.files.getlist("source_file")
+#         for source_file in source_files:
+#             source = PSDImage.open(source_file)
+            
+#             if source:
+#                 all_layer_info = vars(source)  # 레이어의 정보
+            
+#                 psd_image = PSDImage.open(source_file)
+#                 image = psd_image.compose()
+
+#                 for layer in psd_image.descendants(include_clip=True):
+#                     all_extracted_info_list.append(
+#                         {
+#                             "kind": layer.kind,
+#                             "name": layer.name,
+#                             "top": layer.top,
+#                             "left": layer.left,
+#                             "size": layer.size,
+#                         }
+#                     )
+
+#                     if layer.kind == "type":
+#                         text = layer.text.replace("\r", "\n")
+#                         extracted_text_data.append(text)
+
+#                         translated_text = translator.translate(
+#                             text, src="ko", dest="en"
+#                         ).text
+#                         translated_text_data.append(translated_text)
+                        
+#                 original_filename = secure_filename(os.path.splitext(source_file.filename)[0])
+#                 output_image_path = f"static/images/{original_filename}.png"
+#                 image.save(output_image_path)
+                
+#                 image_data = output_image_path
+
+#     except Exception as e:
+#         traceback.print_exc()
+#         return render_template(
+#             "index.html", all_layer_info=f"Error extracting text: {str(e)}"
+#                 )
+
+#     return render_template(
+#         "index.html",
+#         image_data=image_data,
+#         all_layer_info=all_layer_info,
+#         all_extracted_info_list=all_extracted_info_list,
+#         extracted_text_data=extracted_text_data,
+#         translated_text_data=translated_text_data,
+#     )
