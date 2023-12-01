ab09dcd89d0887e4a7cec4d17a8497d558596c3d
Changju Mun <changju7857@gmail.com>
2023-12-01 09:35:12 +0900

    23.11.30 사용자 입력 값으로 번역 구현

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, json, render_template, request, session, send_file
+from flask import Flask, render_template, request, session, send_file
 from googletrans import Translator
 from psd_tools import PSDImage
 from PIL import Image, ImageDraw, ImageFont
